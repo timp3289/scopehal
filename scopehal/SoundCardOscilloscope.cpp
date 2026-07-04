1a67032579fcbee5d0408d019e49aec736eccff1
Tim Pattinson <tpattinson8@gmail.com>
2026-07-04 22:02:49 +1000

    Initial shell of SoundCardOscilloscope based on DemoOscilloscope. The DemoOscilloscope functionality has not been removed yet. Able to enumerate audio devices using PortAudio

---
+++ scopehal/SoundCardOscilloscope.cpp
@@ -0,0 +1,645 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ngscopeclient                                                                                                        *
+*                                                                                                                      *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Implementation of SoundCardOscilloscope
+
+	@ingroup scopedrivers
+ */
+
+#include "scopehal.h"
+#include "OscilloscopeChannel.h"
+#include "SoundCardOscilloscope.h"
+
+#include <portaudio.h>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Construction / destruction
+
+/**
+	@brief Initialize the driver
+
+	@param transport	SCPINullTransport since this driver does not connect to physical hardware
+ */
+SoundCardOscilloscope::SoundCardOscilloscope(SCPITransport* transport)
+	: SCPIDevice(transport, false)
+	, SCPIInstrument(transport, false)
+	, m_extTrigger(nullptr)
+{
+	for(int i=0; i<4; i++)
+	{
+		m_rng[i] = new minstd_rand(m_rd());
+		m_source[i] = new TestWaveformSource(*m_rng[i]);
+	}
+
+	m_model = "Audio Capture";
+	m_vendor = "Generic";
+	m_serial = "";
+
+	PaError pa_err = Pa_Initialize();
+	if(pa_err==paNoError){
+		LogDebug("SoundCardOscilloscope: PulseAudio initialised\n");
+	}else{
+		LogError("SoundCardOscilloscope: PulseAudio init failed with error: %d\n",pa_err);
+	}
+
+	int numPADevices = Pa_GetDeviceCount();
+	
+	LogDebug("SoundCardOscilloscope: enumerating %d devices\n",numPADevices);
+
+	
+
+	int discarded = 0;
+
+	for(int i = 0; i<numPADevices;i++){
+		PaDeviceInfo* pa_dev = (PaDeviceInfo*)Pa_GetDeviceInfo(i);
+
+		if(pa_dev->maxInputChannels > 0){
+			// only keep devices with inputs in our list
+			pa_devices.push_back(pa_dev);
+		}else{
+			discarded++;
+		}
+		
+	}
+	if(discarded > 0 ) LogDebug("SoundCardOscilloscope: discarded %d devices with zero inputs.\n",discarded);
+	
+	LogDebug("SoundCardOscilloscope: kept %zu devices which have inputs.\n",pa_devices.size());
+
+	for(const auto& device: pa_devices){
+		
+		LogDebug("PA device: %s\n\tInput Channels: %d\n\tdefaultLowOutputLatency: %f\n\tdefaultHighInputLatency: %f\n\tdefaultSampleRate %f\n",
+			device->name,device->maxInputChannels,device->defaultLowInputLatency,device->defaultHighInputLatency,device->defaultSampleRate);
+	}
+
+
+	//Create a bunch of channels
+	static const char* colors[8] =
+	{ "#ffff00", "#ff6abc", "#00ffff", "#00c100", "#d7ffd7", "#8482ff", "#ff0000", "#ff8000" };
+
+	for(size_t i=0; i<4; i++)
+	{
+		m_channels.push_back(
+			new OscilloscopeChannel(
+				this,
+				string("CH") + to_string(i+1),
+				colors[i],
+				Unit(Unit::UNIT_FS),
+				Unit(Unit::UNIT_VOLTS),
+				Stream::STREAM_TYPE_ANALOG,
+				i));
+
+		//initial configuration is 1V p-p for each
+		m_channelsEnabled[i] = true;
+		m_channelCoupling[i] = OscilloscopeChannel::COUPLE_DC_50;
+		m_channelAttenuation[i] = 10;
+		m_channelBandwidth[i] = 0;
+		m_channelVoltageRange[i] = 1;
+		m_channelOffset[i] = 0;
+
+		m_channelModes[i] = CHANNEL_MODE_NOISE_LPF;
+	}
+
+	m_sweepFreq = 1e9;
+
+	//Default sampling configuration
+	m_depth = 1e6;
+	m_rate = 50e9;
+
+	m_channels[0]->SetDisplayName("Tone");
+	m_channels[1]->SetDisplayName("Ramp");
+	m_channels[2]->SetDisplayName("PRBS31");
+	m_channels[3]->SetDisplayName("8B10B");
+
+	//Create Vulkan objects for the waveform conversion
+	InitVulkanQueue("SoundCardOscilloscope");
+}
+
+SoundCardOscilloscope::~SoundCardOscilloscope()
+{
+	Pa_Terminate();
+	LogDebug("Shutting down demo scope\n");
+
+	for(int i=0; i<4; i++)
+	{
+		delete m_source[i];
+		delete m_rng[i];
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Information queries
+
+string SoundCardOscilloscope::IDPing()
+{
+	return "";
+}
+
+string SoundCardOscilloscope::GetTransportName()
+{
+	return "null";
+}
+
+string SoundCardOscilloscope::GetTransportConnectionString()
+{
+	return "";
+}
+
+///@brief Return the constant driver name "demopsu"
+string SoundCardOscilloscope::GetDriverNameInternal()
+{
+	return "SoundCard";
+}
+
+unsigned int SoundCardOscilloscope::GetInstrumentTypes() const
+{
+	return INST_OSCILLOSCOPE;
+}
+
+uint32_t SoundCardOscilloscope::GetInstrumentTypesForChannel([[maybe_unused]] size_t i) const
+{
+	return Instrument::INST_OSCILLOSCOPE;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Triggering
+
+Oscilloscope::TriggerMode SoundCardOscilloscope::PollTrigger()
+{
+	if(m_triggerArmed)
+		return TRIGGER_MODE_TRIGGERED;
+	else
+		return TRIGGER_MODE_STOP;
+}
+
+void SoundCardOscilloscope::StartSingleTrigger()
+{
+	m_triggerArmed = true;
+	m_triggerOneShot = true;
+}
+
+void SoundCardOscilloscope::Start()
+{
+	m_triggerArmed = true;
+	m_triggerOneShot = false;
+}
+
+void SoundCardOscilloscope::Stop()
+{
+	m_triggerArmed = false;
+	m_triggerOneShot = false;
+}
+
+void SoundCardOscilloscope::ForceTrigger()
+{
+	StartSingleTrigger();
+}
+
+bool SoundCardOscilloscope::IsTriggerArmed()
+{
+	return m_triggerArmed;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Serialization
+
+void SoundCardOscilloscope::LoadConfiguration(int version, const YAML::Node& node, IDTable& table)
+{
+	//Load the channels
+	auto& chans = node["channels"];
+	for(auto it : chans)
+	{
+		auto& cnode = it.second;
+
+		//Allocate channel space if we didn't have it yet
+		size_t index = cnode["index"].as<int>();
+		if(m_channels.size() < (index+1))
+			m_channels.resize(index+1);
+
+		//Configure the channel
+		Stream::StreamType type = Stream::STREAM_TYPE_PROTOCOL;
+		string stype = cnode["type"].as<string>();
+		if(stype == "analog")
+			type = Stream::STREAM_TYPE_ANALOG;
+		else if(stype == "digital")
+			type = Stream::STREAM_TYPE_DIGITAL;
+		else if(stype == "trigger")
+			type = Stream::STREAM_TYPE_TRIGGER;
+		auto chan = new OscilloscopeChannel(
+			this,
+			cnode["name"].as<string>(),
+			cnode["color"].as<string>(),
+			Unit(Unit::UNIT_FS),
+			Unit(Unit::UNIT_VOLTS),
+			type,
+			index);
+		m_channels[index] = chan;
+
+		//Create the channel ID
+		table.emplace(cnode["id"].as<int>(), chan);
+	}
+
+	//Call the base class to configure everything
+	Oscilloscope::LoadConfiguration(version, node, table);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Channel configuration. Mostly trivial stubs.
+
+bool SoundCardOscilloscope::IsChannelEnabled(size_t i)
+{
+	return m_channelsEnabled[i];
+}
+
+void SoundCardOscilloscope::EnableChannel(size_t i)
+{
+	m_channelsEnabled[i] = true;
+}
+
+void SoundCardOscilloscope::DisableChannel(size_t i)
+{
+	m_channelsEnabled[i] = false;
+}
+
+OscilloscopeChannel::CouplingType SoundCardOscilloscope::GetChannelCoupling(size_t i)
+{
+	return m_channelCoupling[i];
+}
+
+vector<OscilloscopeChannel::CouplingType> SoundCardOscilloscope::GetAvailableCouplings(size_t /*i*/)
+{
+	vector<OscilloscopeChannel::CouplingType> ret;
+	ret.push_back(OscilloscopeChannel::COUPLE_DC_50);
+	return ret;
+}
+
+void SoundCardOscilloscope::SetChannelCoupling(size_t i, OscilloscopeChannel::CouplingType type)
+{
+	m_channelCoupling[i] = type;
+}
+
+double SoundCardOscilloscope::GetChannelAttenuation(size_t i)
+{
+	return m_channelAttenuation[i];
+}
+
+void SoundCardOscilloscope::SetChannelAttenuation(size_t i, double atten)
+{
+	m_channelAttenuation[i] = atten;
+}
+
+unsigned int SoundCardOscilloscope::GetChannelBandwidthLimit(size_t i)
+{
+	return m_channelBandwidth[i];
+}
+
+void SoundCardOscilloscope::SetChannelBandwidthLimit(size_t i, unsigned int limit_mhz)
+{
+	m_channelBandwidth[i] = limit_mhz;
+}
+
+float SoundCardOscilloscope::GetChannelVoltageRange(size_t i, size_t /*stream*/)
+{
+	return m_channelVoltageRange[i];
+}
+
+void SoundCardOscilloscope::SetChannelVoltageRange(size_t i, size_t /*stream*/, float range)
+{
+	m_channelVoltageRange[i] = range;
+}
+
+bool SoundCardOscilloscope::IsHighRateOffsetCapable([[maybe_unused]] size_t i)
+{
+	return true;
+}
+
+OscilloscopeChannel* SoundCardOscilloscope::GetExternalTrigger()
+{
+	return m_extTrigger;
+}
+
+float SoundCardOscilloscope::GetChannelOffset(size_t i, size_t /*stream*/)
+{
+	return m_channelOffset[i];
+}
+
+void SoundCardOscilloscope::SetChannelOffset(size_t i, size_t /*stream*/, float offset)
+{
+	m_channelOffset[i] = offset;
+}
+
+vector<uint64_t> SoundCardOscilloscope::GetSampleRatesNonInterleaved()
+{
+	uint64_t k = 1000;
+	uint64_t m = k * k;
+	uint64_t g = k * m;
+
+	vector<uint64_t> ret;
+	ret.push_back(1 * g);
+	ret.push_back(5 * g);
+	ret.push_back(10 * g);
+	ret.push_back(25 * g);
+	ret.push_back(50 * g);
+	ret.push_back(100 * g);
+	ret.push_back(200 * g);
+	ret.push_back(500 * g);
+	return ret;
+}
+
+vector<uint64_t> SoundCardOscilloscope::GetSampleRatesInterleaved()
+{
+	//no-op
+	vector<uint64_t> ret;
+	return ret;
+}
+
+set<Oscilloscope::InterleaveConflict> SoundCardOscilloscope::GetInterleaveConflicts()
+{
+	//no-op
+	set<Oscilloscope::InterleaveConflict> ret;
+	return ret;
+}
+
+vector<uint64_t> SoundCardOscilloscope::GetSampleDepthsNonInterleaved()
+{
+	uint64_t k = 1000;
+	uint64_t m = k * k;
+
+	vector<uint64_t> ret;
+	ret.push_back(10 * k);
+	ret.push_back(100 * k);
+	ret.push_back(1 * m);
+	ret.push_back(10 * m);
+	ret.push_back(50 * m);
+	return ret;
+}
+
+vector<uint64_t> SoundCardOscilloscope::GetSampleDepthsInterleaved()
+{
+	//no-op
+	vector<uint64_t> ret;
+	return ret;
+}
+
+uint64_t SoundCardOscilloscope::GetSampleRate()
+{
+	return m_rate;
+}
+
+uint64_t SoundCardOscilloscope::GetSampleDepth()
+{
+	return m_depth;
+}
+
+void SoundCardOscilloscope::SetSampleDepth(uint64_t depth)
+{
+	m_depth = depth;
+}
+
+void SoundCardOscilloscope::SetSampleRate(uint64_t rate)
+{
+	m_rate = rate;
+}
+
+void SoundCardOscilloscope::SetTriggerOffset(int64_t /*offset*/)
+{
+	//FIXME
+}
+
+int64_t SoundCardOscilloscope::GetTriggerOffset()
+{
+	//FIXME
+	return 0;
+}
+
+bool SoundCardOscilloscope::CanInterleave()
+{
+	return false;
+}
+
+bool SoundCardOscilloscope::IsInterleaving()
+{
+	return false;
+}
+
+bool SoundCardOscilloscope::SetInterleaving([[maybe_unused]] bool combine)
+{
+	return false;
+}
+
+void SoundCardOscilloscope::PushTrigger()
+{
+	//no-op
+}
+
+void SoundCardOscilloscope::PullTrigger()
+{
+	//no-op
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Waveform degradation modes
+
+vector<string> SoundCardOscilloscope::GetADCModeNames(size_t /*channel*/)
+{
+	vector<string> ret;
+	ret.push_back("Ideal");
+	ret.push_back("10 mV noise");
+	ret.push_back("10 mV noise + 300mm ISI channel");
+	return ret;
+}
+
+size_t SoundCardOscilloscope::GetADCMode(size_t channel)
+{
+	lock_guard<recursive_mutex> lock(m_mutex);
+	return m_channelModes[channel];
+}
+
+void SoundCardOscilloscope::SetADCMode(size_t channel, size_t mode)
+{
+	lock_guard<recursive_mutex> lock(m_mutex);
+	m_channelModes[channel] = mode;
+}
+
+bool SoundCardOscilloscope::IsADCModeConfigurable()
+{
+	return true;
+}
+
+bool SoundCardOscilloscope::IsADCModePerChannel()
+{
+	return true;
+}
+
+vector<Oscilloscope::AnalogBank> SoundCardOscilloscope::GetAnalogBanks()
+{
+	vector<AnalogBank> ret;
+	for(size_t i=0; i<GetChannelCount(); i++)
+		ret.push_back(GetAnalogBank(i));
+	return ret;
+}
+
+Oscilloscope::AnalogBank SoundCardOscilloscope::GetAnalogBank(size_t channel)
+{
+	AnalogBank bank;
+	bank.push_back(GetOscilloscopeChannel(channel));
+	return bank;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Waveform synthesis
+
+bool SoundCardOscilloscope::AcquireData()
+{
+	if(!m_triggerArmed)
+		return false;
+
+	// prepare all channels to be 'about to download'
+	ChannelsDownloadStarted();
+
+	//Sweeping frequency
+	m_sweepFreq += 1e6;
+	if(m_sweepFreq > 1.5e9)
+		m_sweepFreq = 1.1e9;
+	float sweepPeriod = FS_PER_SECOND / m_sweepFreq;
+
+	//Signal degradations
+	float noise[4] =
+	{
+		0.01, 0.01, 0.01, 0.01
+	};
+	bool lpf2 = false;
+	bool lpf3 = false;
+	{
+		lock_guard<recursive_mutex> lock(m_mutex);
+		for(size_t i=0; i<4; i++)
+		{
+			if(m_channelModes[i] == CHANNEL_MODE_IDEAL)
+				noise[i] = 0;
+		}
+		if(m_channelModes[2] == CHANNEL_MODE_NOISE_LPF)
+			lpf2 = true;
+		if(m_channelModes[3] == CHANNEL_MODE_NOISE_LPF)
+			lpf3 = true;
+	}
+
+	//Generate waveforms
+
+	auto depth = GetSampleDepth();
+	int64_t sampleperiod = FS_PER_SECOND / m_rate;
+	WaveformBase* waveforms[4] = {nullptr};
+	for(int i=0; i<4; i++)
+	{
+		if(!m_channelsEnabled[i])
+			continue;
+
+		// Lambda passed to generate waveform methods to update "download" percentage
+		ChannelsDownloadStatusUpdate(i, InstrumentChannel::DownloadState::DOWNLOAD_IN_PROGRESS, 0.0);
+		switch(i)
+		{
+			case 0:
+				{
+					auto wfm = AllocateAnalogWaveform("NoisySine");
+					waveforms[i] = wfm;
+					m_source[i]->GenerateNoisySinewave(
+						*m_cmdBuf, m_queue, wfm, 0.9, 0.0, 1e6, sampleperiod, depth, noise[0]);
+				}
+				break;
+
+			case 1:
+				{
+					auto wfm = AllocateAnalogWaveform("NoisySineSum");
+					waveforms[i] = wfm;
+					m_source[i]->GenerateNoisySinewaveSum(
+						*m_cmdBuf, m_queue, wfm, 0.9, 0.0, M_PI_4, 1e6, sweepPeriod, sampleperiod, depth, noise[1]);
+				}
+				break;
+
+			case 2:
+				{
+					auto wfm = AllocateAnalogWaveform("PRBS31");
+					waveforms[i] = wfm;
+					m_source[i]->GeneratePRBS31(
+						*m_cmdBuf, m_queue, wfm, 0.9, 96969.6, sampleperiod, depth, lpf2, noise[2]);
+				}
+				break;
+
+			case 3:
+				{
+					auto wfm = AllocateAnalogWaveform("8B10B");
+					waveforms[i] = wfm;
+					m_source[i]->Generate8b10b(
+						*m_cmdBuf, m_queue, wfm, 0.9, 800e3, sampleperiod, depth, lpf3, noise[3]);
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		ChannelsDownloadStatusUpdate(i, InstrumentChannel::DownloadState::DOWNLOAD_FINISHED, 1.0);
+	}
+
+	SequenceSet s;
+	for(int i=0; i<4; i++)
+		s[GetOscilloscopeChannel(i)] = waveforms[i];
+
+	//Timestamp the waveform(s)
+	double now = GetTime();
+	time_t start = now;
+	double tfrac = now - start;
+	int64_t fs = tfrac * FS_PER_SECOND;
+	for(auto it : s)
+	{
+		auto wfm = it.second;
+		if(!wfm)
+			continue;
+
+		wfm->m_startTimestamp = start;
+		wfm->m_startFemtoseconds = fs;
+		wfm->m_triggerPhase = 0;
+	}
+
+	m_pendingWaveformsMutex.lock();
+	m_pendingWaveforms.push_back(s);
+	m_pendingWaveformsMutex.unlock();
+
+	if(m_triggerOneShot)
+		m_triggerArmed = false;
+
+	// Tell the download monitor that waveform download has finished
+	ChannelsDownloadFinished();
+
+	return true;
+}
+
---
+++ scopehal/SoundCardOscilloscope.h
@@ -0,0 +1,206 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* libscopehal                                                                                                          *
+*                                                                                                                      *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Declaration of SoundCardOscilloscope
+	@ingroup scopedrivers
+ */
+
+#ifndef SoundCardOscilloscope_h
+#define SoundCardOscilloscope_h
+
+#include "TestWaveformSource.h"
+#include <random>
+
+#include <portaudio.h>
+
+/**
+	@brief Simulated oscilloscope for demonstrations and testing
+	@ingroup scopedrivers
+
+	Four channels:
+	* 1 GHz sine
+	* 1 GHz sine plus 1.1 - 1.5 GHz ramp
+	* 10.3125 Gbps PRBS-31
+	* 1.25 Gbps 8B/10B K28.5 D16.2 idle sequence
+ */
+class SoundCardOscilloscope : public virtual SCPIOscilloscope
+{
+public:
+	SoundCardOscilloscope(SCPITransport* transport);
+	virtual ~SoundCardOscilloscope();
+
+	//not copyable or assignable
+	SoundCardOscilloscope(const SoundCardOscilloscope& rhs) =delete;
+	SoundCardOscilloscope& operator=(const SoundCardOscilloscope& rhs) =delete;
+
+	virtual std::string IDPing() override;
+
+	virtual std::string GetTransportConnectionString() override;
+	virtual std::string GetTransportName() override;
+	virtual uint32_t GetInstrumentTypesForChannel(size_t i) const override;
+
+	//Channel configuration
+	virtual bool IsChannelEnabled(size_t i) override;
+	virtual void EnableChannel(size_t i) override;
+	virtual void DisableChannel(size_t i) override;
+	virtual OscilloscopeChannel::CouplingType GetChannelCoupling(size_t i) override;
+	virtual void SetChannelCoupling(size_t i, OscilloscopeChannel::CouplingType type) override;
+	virtual std::vector<OscilloscopeChannel::CouplingType> GetAvailableCouplings(size_t i) override;
+	virtual double GetChannelAttenuation(size_t i) override;
+	virtual void SetChannelAttenuation(size_t i, double atten) override;
+	virtual unsigned int GetChannelBandwidthLimit(size_t i) override;
+	virtual void SetChannelBandwidthLimit(size_t i, unsigned int limit_mhz) override;
+	virtual float GetChannelVoltageRange(size_t i, size_t stream) override;
+	virtual void SetChannelVoltageRange(size_t i, size_t stream, float range) override;
+	virtual OscilloscopeChannel* GetExternalTrigger() override;
+	virtual float GetChannelOffset(size_t i, size_t stream) override;
+	virtual void SetChannelOffset(size_t i, size_t stream, float offset) override;
+	virtual bool IsHighRateOffsetCapable(size_t i) override;
+
+	//Triggering
+	virtual Oscilloscope::TriggerMode PollTrigger() override;
+	virtual bool AcquireData() override;
+	virtual void Start() override;
+	virtual void StartSingleTrigger() override;
+	virtual void Stop() override;
+	virtual void ForceTrigger() override;
+	virtual bool IsTriggerArmed() override;
+	virtual void PushTrigger() override;
+	virtual void PullTrigger() override;
+
+	virtual std::vector<uint64_t> GetSampleRatesNonInterleaved() override;
+	virtual std::vector<uint64_t> GetSampleRatesInterleaved() override;
+	virtual std::set<InterleaveConflict> GetInterleaveConflicts() override;
+	virtual std::vector<uint64_t> GetSampleDepthsNonInterleaved() override;
+	virtual std::vector<uint64_t> GetSampleDepthsInterleaved() override;
+	virtual uint64_t GetSampleRate() override;
+	virtual uint64_t GetSampleDepth() override;
+	virtual void SetSampleDepth(uint64_t depth) override;
+	virtual void SetSampleRate(uint64_t rate) override;
+	virtual void SetTriggerOffset(int64_t offset) override;
+	virtual int64_t GetTriggerOffset() override;
+	virtual bool IsInterleaving() override;
+	virtual bool SetInterleaving(bool combine) override;
+	virtual bool CanInterleave() override;
+
+	virtual bool IsADCModeConfigurable() override;
+	virtual bool IsADCModePerChannel() override;
+	virtual std::vector<std::string> GetADCModeNames(size_t channel) override;
+	virtual size_t GetADCMode(size_t channel) override;
+	virtual void SetADCMode(size_t channel, size_t mode) override;
+	virtual std::vector<AnalogBank> GetAnalogBanks() override;
+	virtual AnalogBank GetAnalogBank(size_t channel) override;
+
+	virtual unsigned int GetInstrumentTypes() const override;
+	virtual void LoadConfiguration(int version, const YAML::Node& node, IDTable& idmap) override;
+
+protected:
+
+	///@brief External trigger
+	OscilloscopeChannel* m_extTrigger;
+
+	///@brief Map of channel ID to on/off state
+	std::map<size_t, bool> m_channelsEnabled;
+
+	///@brief Map of channel ID to coupling
+	std::map<size_t, OscilloscopeChannel::CouplingType> m_channelCoupling;
+
+	///@brief Map of channel ID to probe attenuation
+	std::map<size_t, double> m_channelAttenuation;
+
+	///@brief Map of channel ID to bandwidth limit
+	std::map<size_t, unsigned int> m_channelBandwidth;
+
+	///@brief Map of channel ID to vertical scale range
+	std::map<size_t, float> m_channelVoltageRange;
+
+	///@brief Map of channel ID to offset
+	std::map<size_t, float> m_channelOffset;
+
+	///@brief Map of channel ID to ADC mode
+	std::map<size_t, size_t> m_channelModes;
+
+	///@brief ADC mode selectors (used to select the simulated channel)
+	enum ChannelModes
+	{
+		///@brief Ideal waveform with no impairments
+		CHANNEL_MODE_IDEAL,
+
+		///@brief 5 mV RMS AWGN
+		CHANNEL_MODE_NOISE,
+
+		///@brief 5 mV RMS AWGN + 300mm lossy S-parameter channel
+		CHANNEL_MODE_NOISE_LPF
+	};
+
+	///@brief Current frequency within the sweep for channel 2
+	float m_sweepFreq;
+
+	///@brief Memory depth
+	uint64_t m_depth;
+
+	///@brief Sample rate
+	uint64_t m_rate;
+
+	///@brief Random number source for seeding the generators
+	std::random_device m_rd;
+
+	///@brief Random number generators for AWGN synthesis
+	std::minstd_rand* m_rng[4];
+
+	/**
+		@brief Signal sources for each channel
+	 */
+	TestWaveformSource* m_source[4];
+
+private:
+	// PortAudio manages the memory allocated for each PaDeviceInfo
+	std::vector<PaDeviceInfo*> pa_devices;
+
+public:
+	static std::string GetDriverNameInternal();
+
+	//This is intentionally not virtual since it's a static method used by enumeration
+	//cppcheck-suppress duplInheritedMember
+	static std::vector<SCPIInstrumentModel> GetDriverSupportedModels()
+	{
+		return {
+        {"Demo Oscilloscope", {{ SCPITransportType::TRANSPORT_NULL, "" }}}
+        };
+	}
+	OSCILLOSCOPE_INITPROC(SoundCardOscilloscope)
+
+
+};
+
+#endif
+
